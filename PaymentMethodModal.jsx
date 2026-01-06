4c8b30aac711ebc585e51c5863eb99d22be7592a
HatidzaImamovic <hatidza12.imamovic12@gmail.com>
2026-01-06 10:04:32 +0100

    Update PaymentMethodModal.jsx

--- PaymentMethodModal.jsx
+++ PaymentMethodModal.jsx
@@ -1,12 +1,16 @@
+import { useLanguage } from "../contexts/LanguageContext";
+
 export default function PaymentMethodModal({ onClose, onCard, onCash }) {
+  const { t } = useLanguage();
+
   return (
     <div className="modal-overlay">
       <div className="modal-box" style={{ width: "300px" }}>
         <button className="close-btn" onClick={onClose}>×</button>
-        <h2>Select Payment Method</h2>
+        <h2>{t("pay")}</h2>
 
         <button className="ok-btn" onClick={onCard}>
-          Pay by Card
+          {t("pay_by_card")}
         </button>
 
         <button
@@ -14,7 +18,7 @@ export default function PaymentMethodModal({ onClose, onCard, onCash }) {
           style={{ background: "#444", marginTop: "10px" }}
           onClick={onCash}
         >
-          Pay by Cash
+          {t("pay_by_cash")}
         </button>
       </div>
     </div>
